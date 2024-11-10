ce95559de73a20d8dc8601567e6dd80fe97fd375
AishikMondal <85011447+AishikMondal@users.noreply.github.com>
2024-11-11 02:33:18 +0530

    Update Textform.jsx

--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -31,6 +31,7 @@ export default function TextForm(props) {
   return (
     <div>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
       <h1>{props.heading}</h1>
       <div className="mb-5">
         <textarea
@@ -56,7 +57,7 @@ export default function TextForm(props) {
       </p>
       {qrCodeUrl && <img src={qrCodeUrl} alt="QR Code" />}
        <p>
-    <footer class="card-header my-29 mx-0 me-auto badge text-bg-secondary">Devoloped By @Aishik</footer>
+    <footer class=" my-29 mx-0 m badge text-bg-secondary">Devoloped by @Aishik</footer>
   </p>
     </div>
   );
